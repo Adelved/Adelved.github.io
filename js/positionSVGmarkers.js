41e22b49072a7c2606bb993fb72c55e05c972e91
Adelved <adelved.dennis@gmail.com>
2020-11-04 13:08:26 +0100

    test

--- js/positionSVGmarkers.js
+++ js/positionSVGmarkers.js
@@ -2,7 +2,6 @@
 //in order to avoid long svg tags in the html file.
 var buttonDiv = document.querySelectorAll(".buttonDivIndex");
 for (element of buttonDiv) {
-  console.log(element);
   for (button of element.children) {
     button.insertBefore(
       createSVG("img/sted_pil-01.svg", "locationMarker", "locationMarker"),
