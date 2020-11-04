74dba4e6c24c6dd249ab583f6129daf8d53497b5
Adelved <adelved.dennis@gmail.com>
2020-11-04 10:59:06 +0100

    fikset contact form

--- js/popUpForm.js
+++ js/popUpForm.js
@@ -1,5 +1,5 @@
 //popup alert when the form is sent from contact.html
-var sendButton = document.querySelectorAll(".contactFormButton")[0];
+var sendButton = document.getElementById("contactForm");
 
 sendButton.addEventListener("submit", function () {
     alert("Takk for din tilbakemelding!");
